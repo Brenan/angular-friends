f7a6ad4813f7885a338af5c148d2e1bcb9ded431
Brenan Klain <brenanklain@gmail.com>
2013-10-11 10:30:11 -0600

    Gets friend list and adds sort functionality

--- FriendController.js
+++ FriendController.js
@@ -1,4 +1,15 @@
+app.controller("friendController", function($scope){
 
+	$.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json',
+			function(response){
+			$scope.friends= response.results;
+			$scope.$apply();	
+			console.log(response);	
+			}
+		);
+	$scope.searchTerm="";
+
+});
 
 
 
